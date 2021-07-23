5a9a773b386b71c38567dab4dc1d0d59e37430e4
Yash786-rgb <60312970+Yash786-rgb@users.noreply.github.com>
2021-07-24 01:20:59 +0530

    Update aboutus.jsx

--- src/components/Home/aboutus.jsx
+++ src/components/Home/aboutus.jsx
@@ -22,7 +22,7 @@ export default function Aboutus() {
   <Col xs={12} sm={12} md={12} lg={4} style={{
     marginBottom:"30",
   }}>
-      <Image className="aboutImg" src={`../home_page_images/About_us.png`} style = {{"borderRadius" : "200%"}}   thumbnail />
+      <Image className="aboutImg" src={`../home_page_images/About_us.png`} style = {{"borderRadius" : "200%", "height" : "97%"}}   thumbnail />
       </Col>
   <Col xs={12} sm={12} md={12} lg={8}>
     <p  className = "aboutUsText"  style={{textAlign:"justify"},{textSizeAdjust:"auto"},{fontSize : "20 px"}}>The American Institute of Chemical Engineering Chapter at IIT Roorkee, is one of the International Chapters from AIChE. It is a student run community which aims to foster chemical engineering at IIT Roorkee. Getting affiliation from AIChE - Global we have always tried to build a suitable environment for chemical engineering education  and have been able to establish global student network. The journey started in 2016 and now we are headed with a global network of chemical engineers from different parts of the world. We  regularly hold meetings and discussions with our alumni, research fellows and faculties on topics relevant to chemical engineering or use of software with Chemical Engg.<br />
